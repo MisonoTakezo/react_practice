ecfce46c68197ba53e25ad0f8eb8d8ea52fb775b
MisonoTakezo <misonotakezo1213@gmail.com>
2022-03-29 15:33:23 +0900

    componentize HeadLine

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Footer } from "../components/Footer";
+import { HeadLine } from "../components/HeadLine";
 import { Links } from "../components/Links";
 import styles from "../styles/Home.module.css";
 
@@ -13,14 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <HeadLine page="index" />
         <Links />
       </main>
       <Footer />
