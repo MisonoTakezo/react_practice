1489eefc417b3b0833e873cb78702b0636c90b30
MisonoTakezo <misonotakezo1213@gmail.com>
2022-03-30 10:19:58 +0900

    Refactors

--- components/Links.jsx
+++ components/Links.jsx
@@ -1,33 +1,38 @@
 import styles from "./Links.module.css";
 
-const Links = () => (
-  <div className={styles.grid}>
-    <a href="https://nextjs.org/docs" className={styles.card}>
-      <h2>Documentation &rarr;</h2>
-      <p>Find in-depth information about Next.js features and API.</p>
-    </a>
-
-    <a href="https://nextjs.org/learn" className={styles.card}>
-      <h2>Learn &rarr;</h2>
-      <p>Learn about Next.js in an interactive course with quizzes!</p>
-    </a>
-
-    <a
-      href="https://github.com/vercel/next.js/tree/canary/examples"
-      className={styles.card}
-    >
-      <h2>Examples &rarr;</h2>
-      <p>Discover and deploy boilerplate example Next.js projects.</p>
-    </a>
-
-    <a
-      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      className={styles.card}
-    >
-      <h2>Deploy &rarr;</h2>
-      <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-    </a>
-  </div>
-);
-
+const Links = () => {
+  const ITEMS = [
+    {
+      href: "https://nextjs.org/docs",
+      header: "Documentation →",
+      paragraph: "Find in-depth information about Next.js features and API.",
+    },
+    {
+      href: "https://nextjs.org/learn",
+      header: "Learn →",
+      paragraph: "Learn about Next.js in an interactive course with quizzes!",
+    },
+    {
+      href: "https://github.com/vercel/next.js/tree/canary/examples",
+      header: "Examples →",
+      paragraph: "Discover and deploy boilerplate example Next.js projects.",
+    },
+    {
+      href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+      header: "Deploy →",
+      paragraph:
+        "Instantly deploy your Next.js site to a public URL with Vercel.",
+    },
+  ];
+  return (
+    <div className={styles.grid}>
+      {ITEMS.map((item, index) => (
+        <a key={index} href={item.href} className={styles.card}>
+          <h2>{item.header}</h2>
+          <p>{item.paragraph}</p>
+        </a>
+      ))}
+    </div>
+  );
+};
 export { Links };
