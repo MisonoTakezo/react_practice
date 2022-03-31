6f9aab054123fbe58b5f7810cfb9b9cd3bbd9208
MisonoTakezo <misonotakezo1213@gmail.com>
2022-03-31 16:00:21 +0900

    introduce useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useState, useCallback } from "react";
 import Head from "next/head";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
@@ -6,21 +6,19 @@ import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
-  const foo = 1;
-  const handleClick = useCallback((e) => {
-    console.log(e.target);
-    e.preventDefault();
-    alert(foo);
+  const [count, setCount] = useState(0);
+  const handleClick = useCallback(() => {
+    setCount((count) => count + 1);
   }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <a href="/about" onClick={handleClick}>
-        ボタン
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
