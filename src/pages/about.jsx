d7605b691c6f145c49cc42658c1013846c856733
MisonoTakezo <misonotakezo1213@gmail.com>
2022-03-31 14:38:12 +0900

    Introduce useEffect

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  useEffect(() => {
+    document.body.style.backgroundColor = "lightblue";
+    console.log("mounted");
+    return () => {
+      document.body.style.backgroundColor = "";
+      console.log("unmounted");
+    };
+  }, []);
   return (
     <div className={styles.container}>
       <Head>
