34fc5eda03598037d69fccb3e021747535259d10
MisonoTakezo <misonotakezo1213@gmail.com>
2022-04-01 10:48:22 +0900

    add click event, change event

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,8 +7,23 @@ import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const [isShown, setIsShown] = useState(false);
+
   const handleClick = useCallback(() => {
     setCount((count) => count + 1);
+  }, []);
+
+  const handleInput = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value);
+  }, []);
+
+  const handleShow = useCallback(() => {
+    setIsShown((isShown) => !isShown);
   }, []);
 
   useEffect(() => {
@@ -26,8 +41,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShown ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleShow}>{isShown ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleInput} />
       <Main page="index" />
       <Footer />
     </div>
