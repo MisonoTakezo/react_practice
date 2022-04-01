42a0e8345d14a6bc9978aa70167ea4c54764c1af
MisonoTakezo <misonotakezo1213@gmail.com>
2022-04-01 13:46:39 +0900

    introduce custom hooks

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,8 +4,13 @@ import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Main } from "../components/Main";
 import styles from "../styles/Home.module.css";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
 
 export default function Home() {
+  const { count, isShown, handleClick, handleDisplay } = useCounter();
+  const { text, textArr, handleInput, handleSubmit } = useInputArray();
+
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     console.log("mounted");
@@ -20,6 +25,21 @@ export default function Home() {
         <title>About Page</title>
       </Head>
       <Header />
+      {isShown ? (
+        <div>
+          <h1>{count}</h1>
+          <button onClick={handleClick}>加算</button>
+        </div>
+      ) : null}
+      <button onClick={handleDisplay}>{isShown ? "非表示" : "表示"}</button>
+
+      <input type="text" value={text} onChange={handleInput} />
+      <button onClick={handleSubmit}>送信</button>
+      <ul>
+        {textArr.map((text, index) => {
+          return <li key={index}>{text}</li>;
+        })}
+      </ul>
       <Main page="about" />
       <Footer />
     </div>
