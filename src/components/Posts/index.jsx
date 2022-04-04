3d30e03a7b6a19cd996d5c5e8fa6b1388f260cfb
MisonoTakezo <misonotakezo1213@gmail.com>
2022-04-04 11:07:50 +0900

    introduce useReducer

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,30 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useReducer, useCallback } from "react";
 
 const Posts = () => {
+  /*
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  */
+  // const [state, setState] = useState({ data: [], loading: true, error: null });
+  const initialState = { data: [], loading: true, error: null };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "end":
+        return {
+          ...state,
+          loading: false,
+          data: action.data,
+        };
+      case "error":
+        return {
+          ...state,
+          loading: false,
+          error: action.error,
+        };
+    }
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   const getPosts = async () => {
     try {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -11,29 +32,27 @@ const Posts = () => {
       if (!res.ok) {
         throw new Error("エラー発生しています。");
       }
-      setPosts(json);
+      dispatch({ type: "end", data: json });
     } catch (e) {
-      setError(e);
-      console.log(e);
+      dispatch({ type: "error", error: e });
     }
-    setLoading(false);
   };
   useEffect(() => {
     getPosts();
   }, []);
-
-  if (loading) {
+  console.log("foo");
+  if (state.loading) {
     return <div>ロード中です。</div>;
   }
-  if (error) {
-    return <div>{error.message}</div>;
+  if (state.error) {
+    return <div>{state.error.message}</div>;
   }
-  if (posts.length <= 0) {
+  if (state.data.length <= 0) {
     return <div>データが空です。</div>;
   }
   return (
     <ol>
-      {posts.map((post, index) => (
+      {state.data.map((post, index) => (
         <li key={index}>{post.title}</li>
       ))}
     </ol>
